675977d22397c30e5fa30a6b84474ef6551bfdf8
Kent Fredric <kentfredric@gmail.com>
2013-07-03 14:47:29 +1200

    add dualsigning util

---
+++ util/dual_sign.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+use utf8;
+
+use Path::Tiny qw(path);
+
+my $keys = { 
+    new => 'E854324B1366A820',
+    old => 'ECD2C675C102CDA4',
+};
+
+for my $key ( sort keys %{$keys} ) {
+    path('.')->child('99-keyid.' . $key )->spew($keys->{$key});
+    for my $file ( qw( 01_Message.mkdn ) ) {
+        my $source = path('.')->child( $file );
+        my $target = path('.')->child( $file . '-' . $key . '.asc' );
+        system(
+            'gpg', '-u', $keys->{$key}, '-a', '-o', $target->stringify, '--detach-sign', $source->stringify 
+        ) == 0 or die;
+    }
+
+    system(
+        'gpg', '-a', '-o', '99-pubkey.'.$key . '.gpg', '--export', $keys->{$key}
+    ) == 0 or die;
+}
